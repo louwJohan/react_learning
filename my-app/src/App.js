1f20739cd7106b45122f6673d3ceabb323979193
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-01 10:38:39 +0000

    Add first stateless functional component

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+      <FunctionalGreeting />
+      <FunctionalGreeting />
+      <FunctionalGreeting />
+      <FunctionalGreeting />
+      <FunctionalGreeting />
+    </div>
+  );
+}
+
+export default App;
