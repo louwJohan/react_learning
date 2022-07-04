6565c4e321c7e6baadd9f2eb8a53a6d7cb8b5090
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-04 10:42:03 +0000

    Lesson  on changing state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
-    
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Louw"/>
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+            count: 0
+        };
+    }
+
+    countClick(){
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState)
+            console.log("Previous Props:", prevProps)
+            return {
+                count: prevState.count + 1
+            }
+        }) 
+
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState)
+            console.log("Previous Props:", prevProps)
+            return {
+                introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }
+        }, () => {
+            console.log('new state', this.state.introduction);
+            console.log('new state',this.state.buttonText);
+        } );
+        console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.name}, {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.countClick()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting
