c5f1d3a2765b726660337a6870f2a9f639962bc7
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-04 14:44:22 +0000

    Add snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,52 @@
+//rfce function
+//import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rce class
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// rconst creates constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clg 
+// console.log(first)
+
+// clo
+// console.log('first', first)
