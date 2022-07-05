1e0dc61c71283a1df07e32df27fa77abd4bd1920
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-05 11:19:43 +0000

    Add event handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+// import NestingComponents from './components/NestingComponents';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
       <StatefulGreeting greeting="I'm a stateful class component!" name="Louw"/>
+      < EventsFunctional />
+      <EventsClass /> */}
+      {/* <ConditionalRenderingFunctional connected={false} />
+      <ConditionalRenderingClass /> */}
+      {/* <NestingComponents/> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
